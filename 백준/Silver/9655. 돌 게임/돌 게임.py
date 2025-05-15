23f1a1fe2c7f42cc973f633628782ffd13f7d659
sillonjeu <pius0316@naver.com>
2025-05-15 18:07:44 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+if N % 2 == 0:
+    print("CY")
+else:
+    print("SK")
+
+# 상근이가 먼저 가져간다
+# 1 -> 창영 승
+# 2 -> 상근 승
+# 3 -> 창영 승
+# 4 -> 상근 승
+# 5 -> 창영 승
+# 6 -> 상근 승
+# 7 -> 창영 승
+# ... 
