d2bd3c8599a46776ef50099692323a4fb947753a
sillonjeu <pius0316@naver.com>
2025-08-04 21:27:00 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 36992 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,6 +1,5 @@
 import sys
 import string
-import math
 
 alpha = list(string.ascii_lowercase)
 L = int(sys.stdin.readline().strip())
@@ -8,6 +7,6 @@ arr = list(sys.stdin.readline().strip())
 
 result = 0
 for index, item in enumerate(arr):
-    result += (alpha.index(item) + 1) * math.pow(31, index)
+    result += (alpha.index(item) + 1) * (31 ** index)
 
-print(int(result % 1234567891))
+print(result % 1234567891)
