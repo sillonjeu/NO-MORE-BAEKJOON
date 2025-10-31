1cd0453cd086bfb41757660b6a02cf693d331c5f
sillonjeu <pius0316@naver.com>
2025-10-31 15:44:54 +0900

    [level 2] Title: 양궁대회, Time: 1.82 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -0,0 +1,52 @@
+def solution(n, info):
+    result = [-1]
+    MAX = -1
+
+    def BT(arr, count, spot):
+        nonlocal result, MAX
+
+        # 마지막 칸 도달 직전이면, 남은 화살 전부 마지막 칸(0점)에 넣고 채점
+        if spot == len(info) - 1:
+            arr.append(count)       
+            BT(arr, 0, spot + 1)
+            arr.pop()
+            return
+
+        if spot == len(info):
+            Lion = 0
+            Pich = 0
+            for i in range(len(info)):
+                if arr[i] > info[i]:
+                    Lion += (10 - i)
+                elif info[i] != 0:     
+                    Pich += (10 - i)
+
+            if Lion > Pich and Lion - Pich > MAX:
+                MAX = Lion - Pich
+                result = arr[:]
+            elif Lion > Pich and Lion - Pich == MAX:
+                # 낮은 점수(뒤 인덱스)부터 비교
+                better = False
+                for i in range(len(arr) - 1, -1, -1):
+                    if arr[i] > result[i]:
+                        better = True
+                        break
+                    elif arr[i] < result[i]:
+                        break
+                if better:
+                    result = arr[:]
+            return
+
+        # 선택 1) 이 과녁에 쏠 수 있으면 쏜다
+        if info[spot] < count:
+            arr.append(info[spot] + 1)
+            BT(arr, count - (info[spot] + 1), spot + 1)
+            arr.pop()
+
+        # 선택 2) 이 과녁엔 0발
+        arr.append(0)
+        BT(arr, count, spot + 1)
+        arr.pop()
+
+    BT([], n, 0)
+    return result
